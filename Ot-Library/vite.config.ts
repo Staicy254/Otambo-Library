6a7192c15ef421511860a003104da92ecd35f0fd
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-03-20 19:11:56 +0300

    json-and vite

--- Ot-Library/vite.config.ts
+++ Ot-Library/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Otambo-Library/", // Ensure this matches your GitHub repository name
   server: {
     host: "::",
     port: 8080,
