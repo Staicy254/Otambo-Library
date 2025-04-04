cd353ddb0ef6fca4242c3106f3344406b4f3410d
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-04-04 19:00:10 +0300

    updates

--- Portfolio/src/components/projects/Projects.jsx
+++ Portfolio/src/components/projects/Projects.jsx
@@ -11,9 +11,9 @@ import image5 from '../../assets/image5.png'
 export default function Projects() {
     const listProjects = [
         {id:1, image:image, title:'ProsperMinds Limited - Online LMS', description:'An online Learning Management System.', link:'https://prospermindslive.com/'},
-        {id:2, image:image2, title:'Parental Lock - Chrome Extension', description:'Chrome extension for locking apps with time limits.', link:'https://chrome.google.com/webstore/detail/parental-control-website/'},
+        {id:2, image:image2, title:'Dukani Kwetu - E-commerce store', description:'An e-commerce store for facial products', link:'https://thriving-environment-962406.framer.app/'},
         {id:3, image:image3, title:'Zendawa Africa - E-commerce Store', description:'An e-commerce store for African products.', link:'https://staicy254.github.io/Zendawa_Africa/'},
-        {id:4, image:image4, title:'Meal Sprint - Food Delivery site', description:'A food delivery site.', link:'https://staicy254.github.io/Meal-Sprint/'},
+        {id:4, image:image4, title:'Tukicode - Online registeration platform', description:'A Program selling site.', link:'https://stupendous-cues-553172.framer.app/'},
         {id:5, image:image5, title:'Cucus Eatery - Restaurant Menu', description:'A restaurant menu.', link:'https://staicy254.github.io/Cucu-s_Eatery/'},
     ];
     
